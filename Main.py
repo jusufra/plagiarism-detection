9862867017530672a273af1edc42da4a5aba45a1
Jusuf <jusuf.ra@gmail.com>
2020-01-05 21:56:38 +0100

    created separate data folder

--- Main.py
+++ Main.py
@@ -2,24 +2,41 @@ import os
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
-PL_FILE = os.path.join(os.getcwd(), "bulhuntr.txt")  # this is the chosen file which is to be checked for plagiarism
-TXT_FOLDER = os.path.join(os.getcwd(), "texts")  # this folder contains the files which are to be compared with the chosen file
+DATA_FOLDER = os.path.join(os.getcwd(), "data")
+REF_FOLDER = os.path.join(DATA_FOLDER, "reference texts")
+FILE_NAME = "huntrprint.txt"
 
-def check_for_plagiarism(text, reference_texts, ngram_length=4):
+
+def create_reference_dict(folder):
+
+    comparison_files = {}
+    for comp_file in os.listdir(folder):
+        flp = os.path.join(folder, comp_file)
+        with open(flp, 'r') as fl:
+            comparison_files[comp_file] = fl.read()
+
+    return comparison_files
+
+
+def check_for_plagiarism(text, ref_texts, ngram_length=4):
     """
-    :param text: list with a single element, which is the content of the chosen text file to be checked for plagiarism
-    :param reference_texts: dictionary with key-value pairs being name and content of text files which are compared to the chosen file
+    :param text: name of the chosen text file to be checked for plagiarism
+    :param ref_texts: dictionary with key-value pairs being name and content of text files which are compared to the chosen file
+    :param ngram_length: length of n-grams to be compared
     :return: list of names of text files which were plagiarized
     """
+    filepath = os.path.join(DATA_FOLDER, text)
+    with open(filepath, 'r') as pl:
+        text = [pl.read()]
+
     n_words = len(text[0].split())
 
     vectorizer = CountVectorizer(ngram_range=(ngram_length, ngram_length))
     vectorizer.fit(text)
-
-    y = vectorizer.transform(reference_texts.values())
+    y = vectorizer.transform(ref_texts.values())
 
     n_overlaps = np.sum(y.toarray(), axis=1)
-    files_to_overlaps = dict(zip(reference_texts.keys(), n_overlaps))
+    files_to_overlaps = dict(zip(ref_texts.keys(), n_overlaps))
 
     treshold = int((0.1*n_words)/ngram_length)
     final_list = [name for name in files_to_overlaps.keys() if files_to_overlaps[name] > treshold]
@@ -28,16 +45,9 @@ def check_for_plagiarism(text, reference_texts, ngram_length=4):
 
 if __name__ == "__main__":
 
-    with open(PL_FILE, 'r') as pl:
-        file = [pl.read()]
+    reference_texts = create_reference_dict(REF_FOLDER)
 
-    comparison_files = {}
-    for comp_file in os.listdir(TXT_FOLDER):
-        fl = os.path.join(TXT_FOLDER, comp_file)
-        with open(fl, 'r') as fl:
-            comparison_files[comp_file] = fl.read()
-
-    names = check_for_plagiarism(file, comparison_files)
+    names = check_for_plagiarism(FILE_NAME, reference_texts)
 
     print(names)
 
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # plagiarism-detection
-run Main.py
+Instructions:
+- file to be checked for plagiarism must be located in 'data' folder
+- the chosen file is compared against all files located in 'data/reference texts' folder
+- to replace the chosen file, also change FILE_NAME in Main.py
+- run Main.py
